a3ef9f090a9f5adf86fa1567d7c0233aa7bb0fd8
Muhammed ilan <muhammed.ilan.se@gmail.com>
2025-05-03 19:45:25 +0300

    Added CRUD operations and DTOs for products

---
+++ Core/ECommerceAPI.Application/DTOs/Product/UpdateProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceAPI.Application.DTOs.Product
+{
+    public class UpdateProductDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public float Price { get; set; }
+    }
+}
--- Presentation/ECommerce.Api/Controllers/ProductsController.cs
+++ Presentation/ECommerce.Api/Controllers/ProductsController.cs
@@ -1,5 +1,8 @@
-﻿using ECommerceAPI.Application.Repositories;
+﻿using ECommerceAPI.Application.DTOs.Product;
+using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace ECommerce.Api.Controllers
 {
@@ -9,5 +12,54 @@ namespace ECommerce.Api.Controllers
     {
         private readonly IProductWriteRepository _productWriteRepository = _productWriteRepository;
         private readonly IProductReadRepository _productReadRepository = _productReadRepository;
-    }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_productReadRepository.GetAll(false));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            return Ok(_productReadRepository.GetByIdAsync(id, false));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateProductDto model)
+        {
+            await _productWriteRepository.AddAsync(new()
+            {
+                Name = model.Name,
+                Stock = model.Stock,
+                Price = model.Price,
+            });
+            await _productWriteRepository.SaveAsync();
+
+            return StatusCode((int)HttpStatusCode.Created);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(UpdateProductDto model)
+        {
+            Product product = await _productReadRepository.GetByIdAsync(model.Id);
+
+            product.Name = model.Name;
+            product.Stock = model.Stock;
+            product.Price = model.Price;
+
+            await _productWriteRepository.SaveAsync();
+            
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _productWriteRepository.RemoveAsync(id);
+            await _productWriteRepository.SaveAsync();
+
+            return Ok();
+        }
+    };
 }
