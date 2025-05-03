685018f4d6fe405c41fb9dafd70cb152f95fdec2
Muhammed ilan <muhammed.ilan.se@gmail.com>
2025-05-03 17:10:58 +0300

    Entity Framework Core Tracking Optimization was done in ReadRepository

--- Infrastructure/ECommerceAPI.Persistence/Repositories/ReadRepository.cs
+++ Infrastructure/ECommerceAPI.Persistence/Repositories/ReadRepository.cs
@@ -11,14 +11,44 @@ namespace ECommerceAPI.Persistence.Repositories
         private readonly ECommerceAPIDbContext _context = _context;
         public DbSet<T> Table => _context.Set<T>();
 
-        public IQueryable<T> GetAll()
-            => Table;
-        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method)
-            => Table.Where(method);
-        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method)
-            => await Table.FirstOrDefaultAsync(method);
+        public IQueryable<T> GetAll(bool tracking = true)
+        {
+            var query = Table.AsQueryable();
 
-        public async Task<T> GetByIdAsync(string id)
-            => await Table.FindAsync(Guid.Parse(id));
+            if (!tracking)
+                query = query.AsNoTracking();
+
+            return query;
+        }
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true)
+        {
+            var query = Table.Where(method);
+            
+            if (!tracking)
+                query = query.AsNoTracking();
+            
+            return query;
+        }
+
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            
+            if (!tracking)
+                query = query.AsNoTracking();
+
+            return await query.FirstOrDefaultAsync(method);
+        }
+
+        public async Task<T> GetByIdAsync(string id, bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+
+            if (!tracking)
+                query = query.AsNoTracking();
+
+            return await query.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+        }
     }
 }
